6f7fe4e4a0dbf2da603bc69a774d521b2001318f
John Pescatore <jdpescat@us.ibm.com>
2017-08-06 12:54:02 -0500

    Added comments for pin numbers

--- src/defaults.h
+++ src/defaults.h
@@ -6,8 +6,8 @@
 #define	P_SCK	B,5
 
 //#define	MCP2515_CS			D,3	// Rev A
-#define	MCP2515_CS			B,2 // Rev B
-#define	MCP2515_INT			D,2
+#define	MCP2515_CS			B,2 //pin 10 // Rev B
+#define	MCP2515_INT			D,2 //pin 2
 #define LED2_HIGH			B,0
 #define LED2_LOW			B,0
 
